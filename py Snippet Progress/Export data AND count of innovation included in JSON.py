c723ee8dba2660a8aa8b8bd4be96adb771644b90
Josh Strupp <js@isl.co>
2020-03-15 13:25:32 -0400

    Create Export data AND count of innovation included in JSON.py

---
+++ py Snippet Progress/Export data AND count of innovation included in JSON.py
@@ -0,0 +1,50 @@
+#NOTE: there are simple JSON to CSV converters: https://json-csv.com
+
+# ------------------------------------------
+
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+from IPython.display import HTML
+import requests
+from urllib import request, response, error, parse
+from urllib.request import urlopen
+import json
+
+urls = ['https://www.aboutamazon.com/our-company', 'https://www.amerisourcebergen.com/about-who-we-are', 
+       'https://www.unitedhealthgroup.com/about']
+data = {}
+
+for url in urls:
+    
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    innovation_instances = soup.find_all(string=re.compile("inno*"))
+    
+    # add count of innovation words to dictionary and print as JSON/CSV element under 
+    
+    data[url] = innovation_instances
+    
+    #Convert innovation_instances to string and count instances
+    
+    def listToString(s):  
+        ii_string = " " 
+
+        # return string   
+        return (ii_string.join(innovation_instances)) 
+
+    string = listToString(s)
+    sub = 'inno'
+    
+    inno_string = (string.count(sub))
+    
+    print (inno_string)
+    
+    innovation_instances.append(inno_string)
+    
+    
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
+    
+  
+print(data)
